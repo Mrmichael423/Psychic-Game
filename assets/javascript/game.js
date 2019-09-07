298eef12135f9261789b0c930d05872478eff972
Kevin Halstead <mrmichael423@icloud.com>
2019-09-07 17:05:37 -0600

    Fixed js errors by adding more var's like in the RPS activity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,34 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
+// console.log(wins)
 var losses = 0;
+// console.log(losses)
 var guesses = 12;
+// console.log(guesses)
 var letterGuessed = [];
-var computerChoice = letters[Math.floor(math.random() * letters.length)];
-//  console.log(computerChoice)
+// console.log(letterGuessed)
+var winText = document.getElementById("win");
+
+var lossText = document.getElementById("loss");
+
+var guessText = document.getElementById("guess");
+
+var leftText = document.getElementById("left");
+
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+ console.log(computerChoice)
 
  document.onkeypress = function(event) {
     var userGuess = event.key;
-    // console.log(userGuess)
+    console.log(userGuess)
     if(userGuess === computerChoice) {
         wins++
     } else {
+        letterGuessed.includes(userGuess) 
         guesses--;
-        userGuess.push(letterGuessed) 
     } if (guesses = 0) {
         losses++
     }
-    
-}
+};
+
