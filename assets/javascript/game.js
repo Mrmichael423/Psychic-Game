3f61ebc1a5643792a67357a1ec81b14e4af22f7a
Kevin Halstead <mrmichael423@icloud.com>
2019-09-07 15:12:44 -0600

    added my ifs and elses to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,20 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var guesses = 12;
+var letterGuessed = [];
+var computerChoice = letters[Math.floor(math.random() * letters.length)];
+//  console.log(computerChoice)
 
+ document.onkeypress = function(event) {
+    var userGuess = event.key;
+    // console.log(userGuess)
+    if(userGuess === computerChoice) {
+        wins++
+    } else {
+        guesses--;
+        userGuess.push(letterGuessed) 
+    } if (guesses = 0) {
+        losses++
+    }
+    
+}
