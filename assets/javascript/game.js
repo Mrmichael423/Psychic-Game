9a73ec9bc327934c26d53bc13156d2b433f09400
Kevin Halstead <mrmichael423@icloud.com>
2019-09-07 22:22:42 -0600

    game now functions 100 precent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,40 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
-console.log(wins)
+// console.log("wins: " + wins)
 var losses = 0;
-// console.log(losses)
+// console.log("losses: " + losses)
 var guesses = 12;
-// console.log(guesses)
+// console.log("guesses: " + guesses)
 var letterGuessed = [];
-// console.log(letterGuessed)
-var winText = document.getElementById("win");
-
-var lossText = document.getElementById("loss");
-
-var guessText = document.getElementById("guess");
-
-var leftText = document.getElementById("left");
-
-var computerChoice = letters[Math.floor(Math.random() * letters.length)];
-console.log(computerChoice)
-
-
- document.onkeypress = function(event) {
+// console.log("guessed letters: " + letterGuessed)
+// the game its self
+document.onkeypress = function(event) {
+    var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+    // console.log("computer choice " + computerChoice)
     var userGuess = event.key;
     letterGuessed.push(userGuess)
     // console.log(userGuess)
-   
+    
     if (userGuess === computerChoice) {
         wins++;
+        guesses = 12;
+        letterGuessed = [];
     } else {
         guesses--;
-    } if (guesses === 0)
+    } if (guesses === 0) {
     losses++;
+    guesses = 12;
+    letterGuessed = []
+    }
+    // documents to the page
+    document.getElementById("win").innerHTML = "Wins: " + wins;
     
+    document.getElementById("loss").innerHTML = "Losses: " + losses;
     
-    winText.textContent = "Wins: " + wins;
-    
-    lossText.textContent = "Losses: " + losses;
-    
-    guessText.textContent = "Guesses left: " + guesses;
+    document.getElementById("guess").innerHTML = "Guesses left: " + guesses;
     
     for (var i = 0; i < letterGuessed.length; i++) {
-    leftText.textContent = "Letters guessed: " + letterGuessed;}
+    document.getElementById("left").innerHTML = "Letters guessed: " + letterGuessed;}
 
 };
 
